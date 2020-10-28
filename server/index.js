baffdf2df0b9288cd5b2ff59f71feb3f0d7bd4f0
ahmedijar92@gmail.com <ahmedijar92@gmail.com>
2020-10-28 09:53:26 +0600

    new: setup  expressJS server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require("./routes")(app);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/api/index.ts
+++ src/api/index.ts
@@ -3,16 +3,20 @@ import { ICommand, IBackendResponse } from "../interfaces";
 export const sendCommand = ({
   command,
 }: ICommand): Promise<IBackendResponse> => {
-  return new Promise((resolve) => {
+  return new Promise(async (resolve) => {
     console.log("Executing command: ", command);
+    const response = await fetch("http://localhost:5000/api/hello");
+    const body = await response.json();
+    console.log(body);
     const sampleResponse = {
       status: 200,
-      result: `Command ${command} executed successfully`,
+      result: body.express,
     };
 
-    setTimeout(() => {
-      console.log("Finished command: ", command);
-      resolve(sampleResponse);
-    }, 3000);
+    resolve(sampleResponse);
+    // setTimeout(() => {
+    //   console.log("Finished command: ", command);
+    //   resolve(sampleResponse);
+    // }, 3000);
   });
 };
